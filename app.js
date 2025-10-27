24ccd51bcf91642b63ab2e5b58ebce6e108db38a
samiullahDeveloper786 <samiullahdeveloperr@gmail.com>
2025-10-27 18:13:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("Ans-1 Asalam O Alikum");
+
+function showAlert2 () {
+    alert("I am learning mern stack Development");
+}
+
+function showAlert3 () {
+    alert("Well Come To JS Land...\nHappy Coding!");
+}
+
+function showAlert4 () {
+    alert("Well Come To JS Land");
+    alert("Happy Coding! \nPrevent this page from creating additional dialogs.");
+}
+
+function showAlert5 () {
+    alert("Hello....I can run JS throught my web browzer's console");
+    console.log(alert);
+}
+
+function showAlert6 () {
+    alert("The Anser is d");
+    console.log(alert);
+}
